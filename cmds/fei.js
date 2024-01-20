07fed5ace78478002aef145c9c99e7fe15589655
jerome123204 <154974042+jerome123204@users.noreply.github.com>
2024-01-20 16:47:46 +0800

    Update fei.js

--- cmds/fei.js
+++ cmds/fei.js
@@ -1,32 +1,31 @@
 const axios = require('axios');
+const fs = require('fs');
 const request = require('request');
+const path = require('path');
 
-async function fei(event, api) {
+async function lyrics(event, api) {
   const input = event.body.toLowerCase().trim();
 
   if (input.includes("-help")) {
     const usage = "Usage: fei [prompt]\n\n" +
-      "Description: ai by kazuma\n\n" +
-      "Example: ai hi";
-      
+      "Description: Ai educ.\n\n" +
+      "Example: lyrics fei what is love";
     api.sendMessage(usage, event.threadID);
     return;
   }
-  
+
   const title = input.slice(7);
 
-  try {
-    const response = await axios.get(`https://scp-09.onrender.com/api/gpt?query=${title}`);
-    const result = response.data.result;
-    const message = `HexaClub ${result}`;
-
-    api.sendMessage({
-      body: message,
-    }, event.threadID);
-  } catch (error) {
-    console.error(error);
-    api.sendMessage("Error while fetching ai. Please try again later.", event.threadID);
-  }
+  axios.get(`https://scp-09.onrender.com/api/gpt?query=${title}`)
+    .then(response => {
+      const result = response.data.result;
+      const message = `Hexaclan ${result.s_result}`;
+      api.sendMessage({ body: message }, event.threadID);
+    })
+    .catch(error => {
+      console.error(error);
+      api.sendMessage("Error while fetching Ai. Please try again later.", event.threadID);
+    });
 }
 
 module.exports = fei;
