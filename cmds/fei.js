b82d80eb949ae65a4241a12890707acaff7bdbef
jerome123204 <154974042+jerome123204@users.noreply.github.com>
2024-01-20 14:19:15 +0800

    Update fei.js

--- cmds/fei.js
+++ cmds/fei.js
@@ -1,37 +1,32 @@
 const axios = require('axios');
-const fs = require('fs');
 const request = require('request');
-const path = require('path');
 
 async function fei(event, api) {
   const input = event.body.toLowerCase().trim();
+
   if (input.includes("-help")) {
     const usage = "Usage: fei [prompt]\n\n" +
       "Description: ai by kazuma\n\n" +
       "Example: ai hi";
+      
     api.sendMessage(usage, event.threadID);
     return;
   }
-
+  
   const title = input.slice(7);
 
-  axios
-    .get(`https://scp-09.onrender.com/api/gpt?query=${title}`)
-    .then(response => {
+  try {
+    const response = await axios.get(`https://scp-09.onrender.com/api/gpt?query=${title}`);
+    const result = response.data.result;
+    const message = `HexaClub "${result.result}"`;
 
-      const result = response.data.result;
-      const message = `HexaClub "${result.result}"`;
-      
-        api.sendMessage({
-          body: message,
-        }, event.threadID);
-      });
-    })
-    .catch(error => {
-      console.error(error);
-      api.sendMessage("Error while fetching lyrics and image. Please try again later.", event.threadID);
-    });
+    api.sendMessage({
+      body: message,
+    }, event.threadID);
+  } catch (error) {
+    console.error(error);
+    api.sendMessage("Error while fetching lyrics and image. Please try again later.", event.threadID);
+  }
 }
 
 module.exports = fei;
-    
