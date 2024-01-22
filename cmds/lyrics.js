c27755ea8433f04292787d3d55ebb2b24e638bd9
jerome123204 <154974042+jerome123204@users.noreply.github.com>
2024-01-22 10:08:02 +0800

    Update lyrics.js

--- cmds/lyrics.js
+++ cmds/lyrics.js
@@ -36,11 +36,10 @@ async function lyrics(event, api, args ) {
         console.error('API response did not contain expected data:', response.data);
         api.sendMessage('❌ An error occurred while generating the text response. Please try again later.', event.threadID, messageID);
       }
-    } .catch(error) {
-      console.error('Error:', error);
-      api.sendMessage('❌ An error occurred while generating the text response. Please try again later.', event.threadID, messageID);
-    }
-  }
-};
+    }  .catch(error => {
+      console.error(error);
+      api.sendMessage("Error. Please try again later.", event.threadID);
+    });
+}
 
 module.exports = lyrics;
